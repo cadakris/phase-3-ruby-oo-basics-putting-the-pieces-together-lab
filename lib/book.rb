a04cbb7625acddf7f74983b44472650e853b6063
Kristen <kriscadacio@gmail.com>
2022-04-05 14:38:49 -0400

    commit tree c6866196d6cc1e47d1adb738f85dad4de6835445

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
 
+class Book
+    attr_accessor :author, :page_count, :genre, :title
+
+    def initialize (title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+#this is creating instance of the book
+new_book = Book.new("My Fucking Awesome Book")
+#we are passing values here
+new_book.author = "Kristen Fucking Cadacio"
+new_book.page_count = 100
+new_book.genre = "Fucking Awesome"
+
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize (brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+new_shoe = Shoe.new("Nike")
+new_shoe.color = "Red"
+new_shoe.size = 7
+new_shoe.material = "Leather"
+new_shoe.condition = "Great"
